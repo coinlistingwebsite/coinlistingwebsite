30d6e06608128febac02c0d940205ea1dcd23fcf
Smart Oghenemine <35765212+VinciDemons@users.noreply.github.com>
2024-06-10 12:06:46 +0100

    update

--- components/main/token-details/chart.js
+++ components/main/token-details/chart.js
@@ -1,53 +1,61 @@
 import React from "react";
-// import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-
-// import { Bar, Doughnut, Line, Pie } from "react-chartjs-2";
-
-// ChartJS.register(ArcElement, Tooltip, Legend);
-
 import {
   Chart as ChartJS,
-  RadialLinearScale,
-  PointElement,
-  LineElement,
-  Filler,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
   Tooltip,
   Legend,
 } from "chart.js";
-import { Radar } from "react-chartjs-2";
+import { Bar } from "react-chartjs-2";
 
 ChartJS.register(
-  RadialLinearScale,
-  PointElement,
-  LineElement,
-  Filler,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
   Tooltip,
   Legend
 );
 
 const DoughnutElement = ({ dataChart }) => {
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Boosts Analysis",
+      },
+    },
+  };
+
+  const labels = ["Boosts"];
+
   const data = {
-    labels: ["Bullish", "Bearish"],
+    labels,
     datasets: [
       {
-        label: "Boosts",
-        data: [dataChart.bullish, dataChart.bearish],
-        backgroundColor: ["rgb(22 163 74 )", "rgb(248 113 113)"],
-        borderColor: ["#FAF9F6"],
-        // backgroundColor: "rgba(255, 99, 132, 0.2)",
-        // borderColor: "rgba(255, 99, 132, 1)",
-        borderWidth: 1,
+        label: "Bullish",
+        data: labels.map(() => dataChart.bullish),
+        backgroundColor: "rgb(22 163 74 )",
+      },
+      {
+        label: "Bearish",
+        data: labels.map(() => dataChart.bearish),
+        backgroundColor: "rgb(248 113 113)",
       },
     ],
   };
-
-  const options = {};
 
   return (
     <div className="w-[100%] p-5">
       {/* <Doughnut data={data} options={options}></Doughnut> */}
 
-      <Radar data={data} />
+      <Bar options={options} data={data} />
     </div>
   );
 };
