085c17d6ffdb486fa3b5aa0ff44ccb1164ef51b6
boostlegends <boostlegends1@gmail.com>
2024-12-23 00:17:14 +0100

    Update page.js

--- app/gainers-losers/page.js
+++ app/gainers-losers/page.js
@@ -1,6 +1,12 @@
 import GainersLosers from "@/components/main/gainers-losers";
 import React from "react";
 
+export const metadata = {
+  title: "Top Crypto Gainers & Losers – Live Market Insights | RankCoins",
+  description:
+    "Stay ahead of the crypto market with RankCoins. Discover today’s biggest price movers—top gainers and top losers—updated in real-time, and make more informed trading decisions.",
+};
+
 export default function GainersLosersPage() {
   return (
     <main className="max-w-[1500px] mx-auto p-2">
