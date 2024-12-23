d1e3525465a025d1c7b6d4f01e07255771d36e68
boostlegends <boostlegends1@gmail.com>
2024-12-23 11:05:36 +0100

    Update index.js

--- components/sub-main/tweets/index.js
+++ components/sub-main/tweets/index.js
@@ -13,12 +13,12 @@ const Tweets = () => {
   return (
     <>
       <span className="flex-1 text-xl font-extrabold">
-        BullishMarketCap Twitter Posts
+        RankCoins Twitter Posts
       </span>
 
       <div className="flex flex-row overflow-x-auto gap-5">
         <Tweet />
-        <Tweet link="https://x.com/arjaheri/status/1776319773816639796" />
+        <Tweet link="https://x.com/RankCoins" />
         <Tweet />
         <Tweet />
         <Tweet />
